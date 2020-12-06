5e7c36bdbb8d5db51ed9481dcc7decaef43b91c7
Gabriel Aversano <g.aversano@live.ca>
2020-12-06 17:21:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
+
+import pandas as pd
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    
+    data = pd.read_csv("madden21_ratings.csv")
+    
+    names = data["Full Name"].unique()
+    teams = data["Teams"].unique()
+    
+    return render_template('index.html', names = names, teams = teams)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
